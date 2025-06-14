728d969ed7771c5645a2763b38a702e78dc2f536
Nabeel Ahmad <Nabeel.Ahmad@alokind.com>
2025-06-14 14:42:00 +0530

    Updated for deployment13

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'https://mini-contact-application.netlify.app'],
     credentials: true
   })
 
